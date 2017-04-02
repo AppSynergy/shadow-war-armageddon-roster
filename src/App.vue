a2b97b5f9bdeb880b9a719528b4d2bf84264b068
Adam Marshall <adam@appsynergy.net>
2017-04-02 21:07:54 +0100

    First

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello, Skitarii!'
+    }
+  }
+}
+</script>
