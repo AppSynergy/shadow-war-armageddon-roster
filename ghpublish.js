73455c9fc013cc18f443cc0abc6e634fdbe6dc39
Adam Marshall <adam@appsynergy.net>
2017-04-24 15:34:30 +0100

    Firebase setup

--- ghpublish.js
+++ ghpublish.js
@@ -1,6 +1,6 @@
 const ghpages = require('gh-pages');
 const path = require('path');
 
-ghpages.publish(path.join(__dirname, 'build'), function() {
+ghpages.publish(path.join(__dirname, 'public'), function() {
   console.log("Published to github.");
 });
